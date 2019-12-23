ac06474194bce54efa80a831ccdd8ffa85902378
Fareed Stevenson <atlass32@com.go.xhome.us>
2019-12-22 21:55:18 -0800

    Add shared configuration file

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,14 +1,15 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-    console.log("document.readyState: " + document.readyState.toString());
+chrome.extension.sendMessage({}, response => {
+	var readyStateCheckInterval = setInterval(() => {
   	if (document.readyState === "complete") {
   		clearInterval(readyStateCheckInterval);
 
   		// ----------------------------------------------------------
   		// This part of the script triggers when page is done loading
-  		console.log("Hello. This message was sent from scripts/inject.js");
-  		// ----------------------------------------------------------
-
-  	}
-	}, 10);
+  		// log to console of foreground tab
+  		if (noisy) {
+        console.log("Hello. This message was sent from scripts/inject.js");
+  		  console.dir(document);
+      }
+    }
+  }, 10);
 });
---
+++ src/shared/config.js
@@ -0,0 +1,1 @@
+const noisy = true;
